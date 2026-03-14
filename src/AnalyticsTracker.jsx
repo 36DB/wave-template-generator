d9d87307cc42516b9458cae07c1dc94e14df3165
36DB <bradje@naver.com>
2026-03-14 16:55:10 +0900

    Analytics implementation

---
+++ src/AnalyticsTracker.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (typeof window.gtag !== "function") return;
+
+    const pagePath = `${location.pathname}${location.search}${location.hash || ""}`;
+
+    window.gtag("event", "page_view", {
+      page_path: pagePath,
+      page_location: window.location.href,
+      page_title: document.title,
+    });
+  }, [location]);
+
+  return null;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import BoardPage from "./BoardPage";
+import AnalyticsTracker from "./AnalyticsTracker";
 import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
+      <AnalyticsTracker />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/board" element={<BoardPage />} />
